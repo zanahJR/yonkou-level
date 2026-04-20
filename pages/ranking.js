5cc4e9061f7a4216ad46fa2a2cfc013974f0388b
zanahJR <zanahmarinjr@gmail.com>
2026-04-20 13:13:52 +0200

    Update ranking.js

--- pages/ranking.js
+++ pages/ranking.js
@@ -1,3 +1,20 @@
+import { useEffect, useState } from 'react';
+
 export default function Ranking() {
-  return <h1>Ranking funcionando</h1>;
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/ranking')
+      .then(res => res.json())
+      .then(data => setPlayers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Ranking</h1>
+      {players.map((p, i) => (
+        <p key={i}>{p.name} - {p.points}</p>
+      ))}
+    </div>
+  );
 }
