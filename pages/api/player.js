9e1d293e9b5231720c0f8365b31d13a9133aa93d
zanahJR <zanahmarinjr@gmail.com>
2026-04-20 16:20:03 +0200

    Update player.js

--- pages/api/player.js
+++ pages/api/player.js
@@ -1,16 +1,20 @@
-import { supabase } from '../../lib/supabase';
+import { supabase } from "../../lib/supabase";
 
 export default async function handler(req, res) {
-  const { data } = await supabase
-    .from('players')
-    .select('*')
+  const { data, error } = await supabase
+    .from("players")
+    .select("*")
     .limit(1)
     .single();
+
+  if (error || !data) {
+    return res.status(500).json({ error: "No data" });
+  }
 
   const level = Math.floor(data.points / 100);
 
   res.status(200).json({
     ...data,
-    level
+    level,
   });
 }
