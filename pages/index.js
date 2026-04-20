8133e5ea27eacdd0e5a5035df8eaade75bb1e395
zanahJR <zanahmarinjr@gmail.com>
2026-04-20 22:02:06 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>FUNCIONA 🔥</h1>
-      <a href="/login">Login</a>
-      <br />
-      <a href="/ranking">Ranking</a>
-    </div>
-  );
+  return <h1>Funciona 🔥</h1>;
 }
