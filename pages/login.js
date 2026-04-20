80b9ee7127d5deb416537b34ba9a592c8f779b19
zanahJR <zanahmarinjr@gmail.com>
2026-04-21 01:07:19 +0200

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,36 +1,3 @@
 export default function Login() {
-  return (
-    <div style={{
-      height: "100vh",
-      background: "black",
-      color: "white",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column"
-    }}>
-      <h1>Login 🏴‍☠️</h1>
-
-      <input
-        placeholder="Usuario"
-        style={{ margin: 10, padding: 10 }}
-      />
-
-      <input
-        placeholder="Contraseña"
-        type="password"
-        style={{ margin: 10, padding: 10 }}
-      />
-
-      <button style={{
-        marginTop: 10,
-        padding: 10,
-        background: "gold",
-        border: "none",
-        cursor: "pointer"
-      }}>
-        Entrar
-      </button>
-    </div>
-  );
+  return <h1>Login</h1>;
 }
