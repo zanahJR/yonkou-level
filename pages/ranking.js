9048964796b0cfd16372ec37a15b3cc459a94b77
zanahJR <zanahmarinjr@gmail.com>
2026-04-21 17:27:15 +0200

    Update ranking.js

--- pages/ranking.js
+++ pages/ranking.js
@@ -1,3 +1,37 @@
 export default function Ranking() {
-  return <h1>Ranking 🏴‍☠️</h1>;
+  const players = [
+    { name: "Luffy", level: 100 },
+    { name: "Zoro", level: 90 },
+    { name: "Sanji", level: 85 }
+  ];
+
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#111",
+      color: "white",
+      padding: 40
+    }}>
+      <h1>🏆 Ranking</h1>
+
+      {players.map((p, i) => (
+        <div key={i} style={{ marginBottom: 20 }}>
+          <div>{p.name}</div>
+
+          <div style={{
+            background: "#333",
+            height: 20,
+            borderRadius: 10
+          }}>
+            <div style={{
+              width: `${p.level}%`,
+              background: "gold",
+              height: "100%",
+              borderRadius: 10
+            }} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
